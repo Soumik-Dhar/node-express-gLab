4b3983f44854a4102c6453b55c8f1f9efa6922d2
Soumik-Dhar <dev.soumik23@gmail.com>
2020-06-25 04:56:10 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 const jsonData = {
   success: true,
@@ -20,6 +22,30 @@ app.get("/users/:user", function(req, res) {
     name: req.params.user
   };
   res.json(jsonData);
+});
+
+app.post("/login", function(req, res) {
+  const user = {
+    username: req.body.username,
+    password: req.body.password
+  };
+  const credentials = {
+    username: "MgOvercast",
+    password: "password-_-"
+  };
+  if ((user.username === credentials.username) && (user.password === credentials.password)) {
+    res.json({
+      success: true,
+      message: "Username and Password match!",
+      token: "<Encrypted token goes here>"
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "Username and Password do not match!",
+      token: "<No token generated>"
+    });
+  }
 });
 
 app.listen(8000, function() {
