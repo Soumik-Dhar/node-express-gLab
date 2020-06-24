d351cf2d40f60f51c81b4d590bf1191cfe768df2
Soumik-Dhar <dev.soumik23@gmail.com>
2020-06-25 04:34:41 +0530

    add dynamic GET route

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const app = express();
 
-const userData = [{
-  name: "Soumik"
-}, {
-  name: "Richik"
-}];
+const jsonData = {
+  success: true,
+  message: "User list updated!",
+  userData: [{
+    name: "Soumik"
+  }, {
+    name: "Richik"
+  }]
+};
 
 app.get("/users", function(req, res) {
-  res.json({
-    success: true,
-    message: "User list obtained!",
-    users: userData
-  });
+  res.json(jsonData);
+});
+
+app.get("/users/:user", function(req, res) {
+  jsonData.userData[2] = {
+    name: req.params.user
+  };
+  res.json(jsonData);
 });
 
 app.listen(8000, function() {
