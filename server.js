506d588e054234107f66aef0ae16689f5a517b72
Soumik-Dhar <dev.soumik23@gmail.com>
2020-06-25 04:22:23 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const userData = [{
+  name: "Soumik"
+}, {
+  name: "Richik"
+}];
+
+app.get("/users", function(req, res) {
+  res.json({
+    success: true,
+    message: "User list obtained!",
+    users: userData
+  });
+});
 
 app.listen(8000, function() {
   console.log("Server is running on port 8000");
